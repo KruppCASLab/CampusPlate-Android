e09c4762ce8a1abfd75851e1e800af2314b5f474
etimko16 <etimko16@mail.bw.edu>
2020-04-09 20:57:59 -0400

    Added ListingModel and Listing type

---
+++ app/src/main/java/com/example/campusplate_android/Model/ListingModel.java
@@ -0,0 +1,49 @@
+package com.example.campusplate_android.Model;
+
+import com.example.campusplate_android.Model.Types.Listing;
+
+import java.util.ArrayList;
+
+public class ListingModel {
+    private ArrayList<Listing> listings = new ArrayList<>();
+    static private ListingModel sharedInstance = null;
+
+    private ListingModel(){
+
+    }
+
+    static synchronized public ListingModel getSharedInstance(){
+        if (sharedInstance == null){
+            sharedInstance = new ListingModel();
+        }
+        return sharedInstance;
+    }
+
+    public Listing getListing(int index){
+        return this.listings.get(index);
+    }
+    public void addListing(Listing listing){
+        this.listings.add(listing);
+    }
+    public void removeListing(Listing listing){
+        this.listings.remove(listing);
+    }
+    public int getNumberOfListings(){
+        return this.listings.size();
+    }
+    public ArrayList<Listing> getAllListings(){
+        return this.listings;
+    }
+
+    public ArrayList<Listing> getUserListings(int userId){
+        ArrayList<Listing> userListings = new ArrayList<>();
+        for (int i = 0; i < this.listings.size(); i++){
+            if (this.listings.get(i).userId == userId){
+                userListings.add(this.listings.get(i));
+            }
+        }
+        return userListings;
+    }
+
+
+}
---
+++ app/src/main/java/com/example/campusplate_android/Model/Types/Listing.java
@@ -0,0 +1,35 @@
+package com.example.campusplate_android.Model.Types;
+
+public class Listing {
+    public int listingId;
+    public int userId;
+    public String title;
+    public String locationDescription;
+    public double lat;
+    public double lng;
+    public int creationTime;
+    public int quantity;
+    //TODO: Add image
+
+    public Listing() {
+        this.listingId = -1;
+        this.userId = -1;
+        this.title = null;
+        this.locationDescription = null;
+        this.lat = 0;
+        this.lng = 0;
+        this.creationTime = 0;
+        this.quantity = 0;
+    }
+
+    public Listing(int listingId, int userId, String title, String locationDescription, double lat, double lng, int creationTime, int quantity) {
+        this.listingId = listingId;
+        this.userId = userId;
+        this.title = title;
+        this.locationDescription = locationDescription;
+        this.lat = lat;
+        this.lng = lng;
+        this.creationTime = creationTime;
+        this.quantity = quantity;
+    }
+}
