427bde50f01a7243f0bac0cbe78fa6856863b40f
jverhosek <jt3enigma@gmail.com>
2024-02-14 22:13:05 -0500

    Location Permission
    Location Permission Started and Gradle updates

--- app/src/main/java/com/example/campusplate_android/MainActivity.java
+++ app/src/main/java/com/example/campusplate_android/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.campusplate_android;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Looper;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
@@ -27,9 +32,10 @@ import androidx.navigation.ui.NavigationUI;
 
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
 
-
+    LocationManager locationManager;
+    String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,21 +53,33 @@ public class MainActivity extends AppCompatActivity {
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 
-
         //Location stuff
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+        provider = locationManager.getBestProvider(new Criteria(), false);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+             // TODO: Consider calling
+             //    ActivityCompat#requestPermissions
+             // here to request the missing permissions, and then overriding
+             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+             //                                          int[] grantResults)
+             // to handle the case where the user grants the permission. See the documentation
+             // for ActivityCompat#requestPermissions for more details.
+             return;
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
 
+        if (location != null) {
+           Log.i("Location Info", "Location achieved!");
 
+        } else {
 
+           Log.i("Location Info", "No location :(");
+
+        }
 
     }
-
-
-
-
-
-
 
 
     @Override
@@ -89,5 +107,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void hideUpButton() {
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        Double lat = location.getLatitude();
+        Double lng = location.getLongitude();
+
+        Log.i("Location info: Lat", lat.toString());
+        Log.i("Location info: Lng", lng.toString());
+
     }
 }
