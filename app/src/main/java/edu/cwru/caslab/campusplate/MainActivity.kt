6944fbeb59c3cb3801cdc4ded25d26fd28c74e6c
Alan Yeung <184780131+alan-ysy@users.noreply.github.com>
2026-05-28 04:06:19 +0000

    Add TextField functionality and add aesthetic changes

--- app/src/main/java/edu/cwru/caslab/campusplate/MainActivity.kt
+++ app/src/main/java/edu/cwru/caslab/campusplate/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
@@ -17,8 +21,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import edu.cwru.caslab.campusplate.ui.theme.CampusPlateTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,23 +48,31 @@ fun CampusPlateLogin(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         var value by remember { mutableStateOf("") }
 
         Text(
-            text = "Campus Plate Login"
+            text = "Campus Plate Login",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold
         )
+        Spacer(modifier = Modifier.height(64.dp))
         TextField(
             value = value,
             singleLine = true,
-            modifier = modifier,
-            onValueChange = {},
-            label = { Text(text = "Username") },
+            modifier = Modifier.fillMaxWidth(0.8f),
+            onValueChange = { value = it },
+            label = { Text(text = "School Email") },
             keyboardOptions = KeyboardOptions.Default
         )
+        Spacer(modifier = Modifier.height(16.dp))
         Button(
-            onClick = {}
+            onClick = {
+                // TODO: Email confirmation logic
+            }
         ) {
             Text(text = "Send PIN")
         }
